5f53b2bc27a93366b4332eb58918059e35f2b563
Zenon-Nowakowski <zenon.nowakowski8@gmail.com>
2023-10-19 08:24:45 -0500

    completed solution

--- main.py
+++ main.py
@@ -1,27 +1,35 @@
+#The class which finds the needle
 def find_needle_in_haystack(haystack, needle):   
-    index = 0 
+    #var declarations to make the function work
+    needle_index = 0 
     solution = ""
-    for i,letter in enumerate(haystack): 
-        print('letter value: {}, needle value: {}, at index: {}'.format(haystack[i], needle[index % len(needle)], index))
-        if needle[index % len(needle)] == haystack[i]:
-            solution += haystack[i]
-            index+=1
-            print('solution: {} index of heystack: {} index of needle: {}'.format(solution, i, index))
+    index = 0
+    #this is functioning almost identically the same as a for loop with the acception that we can manipulate index at any point of the execution 
+    while(index < len(haystack)): 
+        #print('letter value: {}, needle value: {}, at needle_index: {}'.format(haystack[index], needle[needle_index % len(needle)], needle_index))
+        #if a char belonging to the needle is found, then add it to the solution string and increment the index of needle, modulus is used to prevent overflow
+        if needle[needle_index % len(needle)] == haystack[index]:
+            solution += haystack[index]
+            needle_index+=1
+            #print('solution: {} needle_index of heystack: {} needle_index of needle: {}'.format(solution, index, needle_index))
+        #reset the index if needle index and solution vars if solution is not found
         else:
-            i -= index
-            index = 0
+            index -= needle_index
+            needle_index = 0
             solution = ""
-            print('correct char not found, solution: {} index of heystack: {} index of needle: {}'.format(solution, i, index))
+            #print('correct char not found, solution: {} needle_index of heystack: {} needle_index of needle: {}'.format(solution, index, needle_index))
         if(needle == solution):
-            print(solution)
-            return i-len(needle)+1
+            #print(solution)
+            return index-len(needle)+1
+        index+=1
     return -1
-# value = find_needle_in_haystack("leetcode", "leeto")
-# print('returned value:{}'.format(value))
-# value = find_needle_in_haystack("hello", "ll")
-# print('returned value:{}'.format(value))
-# value = find_needle_in_haystack("mississipi", "issi")
-# print('returned value:{}'.format(value))
+# test cases commonly failed: 
+value = find_needle_in_haystack("leetcode", "leeto")
+print('returned value:{}'.format(value))
+value = find_needle_in_haystack("hello", "ll")
+print('returned value:{}'.format(value))
+value = find_needle_in_haystack("mississipi", "issi")
+print('returned value:{}'.format(value))
 #error with this test is that it compares to p, does not find it and resets however the way the logic currently works it compares to the 'ipi' as it has already looped through the solution 
 value = find_needle_in_haystack("mississipi", "issip")
 print('returned value:{}'.format(value))
