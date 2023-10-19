757e44947ccf2109bed059352d3afa8883e35678
Zenon-Nowakowski <zenon.nowakowski8@gmail.com>
2023-10-19 08:03:32 -0500

    found error, attempting to fix by manipulating index

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ def find_needle_in_haystack(haystack, needle):
     index = 0 
     solution = ""
     for i,letter in enumerate(haystack): 
-        if needle[index % len(needle)] == letter:
-            solution += letter
+        print('letter value: {}, needle value: {}, at index: {}'.format(haystack[i], needle[index % len(needle)], index))
+        if needle[index % len(needle)] == haystack[i]:
+            solution += haystack[i]
             index+=1
+            print('solution: {} index of heystack: {} index of needle: {}'.format(solution, i, index))
         else:
-            #print('correct char not found')
+            i -= index
             index = 0
             solution = ""
+            print('correct char not found, solution: {} index of heystack: {} index of needle: {}'.format(solution, i, index))
         if(needle == solution):
             print(solution)
             return i-len(needle)+1
@@ -19,5 +22,6 @@ def find_needle_in_haystack(haystack, needle):
 # print('returned value:{}'.format(value))
 # value = find_needle_in_haystack("mississipi", "issi")
 # print('returned value:{}'.format(value))
+#error with this test is that it compares to p, does not find it and resets however the way the logic currently works it compares to the 'ipi' as it has already looped through the solution 
 value = find_needle_in_haystack("mississipi", "issip")
 print('returned value:{}'.format(value))
